7111847eb3db884557ed160abb366ed93dde39b0
PrijalBista <pri1.zzal@gmail.com>
2019-06-18 20:40:40 +0545

    Refactor with Promise and async/await Promisified the filesystem(fs) mkdir and fileWrite enclosed all asynchronous fs tasks inside an async anonymous function ( async()=>{//code} ) (); directly executed after creation. used await for all Promisified file system(fs) tasks. Error handling using try-catch take argument for project name using process.argv[2]  // 0 and 1 are [node , script-file.js, first arg, second arg, ...]

--- simpleExpressTemplateWithRoutes.js
+++ simpleExpressTemplateWithRoutes.js
@@ -2,115 +2,145 @@ const fs = require("fs");
 
 
 const createDirectory = dirname => {
-
-	if (!fs.existsSync(dirname)){
-	    fs.mkdirSync(dirname);
-	    console.log("Directory: "+dirname+" created");
-	}
+	return new Promise( ( resolve, reject ) => {
+		if(fs.existsSync(dirname)) reject("Directory'"+dirname+"' already Exists");
+		
+		fs.mkdir(dirname, err => {
+			if (err) reject(err);
+			else resolve("Directory: "+dirname+" created");
+		});
+		
+	});
+	
 }
 
 const createFile = ( file, content )  => {
-	
-	let pathToFile = file;
-	fs.stat(pathToFile, "utf8", (err, data) => {
+	return new Promise( ( resolve, reject ) => {
+		let pathToFile = file;
+		fs.stat(pathToFile, "utf8", (err, data) => {
 
-		if(err == null) { console.log("File exists"); return}
-		else if(err.code == "ENOENT"){
-			//file doesnot exist so creating one
-			//check if routeContent is a string
+			if(err == null) reject("File exists");
+			else if(err.code == "ENOENT"){
+				//file doesnot exist so creating one
+				//check if routeContent is a string
 
-			fs.writeFileSync(pathToFile,content);
-			console.log(file+" created");
-		}
-	});
+				fs.writeFile(pathToFile,content, err => {
+					if(err) reject(err);
+					else resolve("FILE: "+file+" created");
+				});
+			}
+		});
+	});	
 }
 
+if(!process.argv[2]){
+	console.log(` ERR:// One Argument missing <project name>
+------------------------------------------------------------------------------------
+	Custom NodeJs + Express template generator :)
 
-PROJECT_NAME = "testing1";
+	Usage: node simpleExpressTemplateWithRoute.js <project-name>
+------------------------------------------------------------------------------------
+	Example:
+	$ node simpleExpressTemplateWithRoute.js myExpressProject
+------------------------------------------------------------------------------------
+	`);
+	process.exit(-1);
+}
 
-//create project directory
-createDirectory(PROJECT_NAME);
+PROJECT_NAME = process.argv[2];
 
-//Create simple package.json file
+( async() => {
+	try {
+		//create project directory
+		console.log(await createDirectory(PROJECT_NAME));
 
-packageJson = {
-  "name": PROJECT_NAME,
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "start": "nodemon index.js"
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "express": "latest",
-    "morgan" : "latest"
-  }
-};
+		//Create simple package.json file
 
-createFile(PROJECT_NAME+"/package.json",JSON.stringify(packageJson, null, 4 ));
+		packageJson = {
+		  "name": PROJECT_NAME,
+		  "version": "1.0.0",
+		  "description": "",
+		  "main": "index.js",
+		  "scripts": {
+		    "start": "nodemon index.js"
+		  },
+		  "author": "",
+		  "license": "ISC",
+		  "dependencies": {
+		    "express": "latest",
+		    "morgan" : "latest"
+		  }
+		};
 
-
-//Create directories for Basic project Structure
-createDirectory(PROJECT_NAME+"/routes")
-createDirectory(PROJECT_NAME+"/public")
-
-//Create index.js
-
-indexFile = `
-const express = require("express");
-const app = express();
-
-const morgan = require("morgan"); //logging -> express.logger is deprecated.
-
-//Routes
-const indexRoute = require("./routes/index");
-const usersRoute = require("./routes/users");
-
-//Middlewares
-app.use(express.json());
-app.use(morgan("tiny"));
-app.use(express.static('public'));
-
-//Route Middlewares
-app.use("/", indexRoute);
-app.use("/users", usersRoute);
+		console.log(await createFile(PROJECT_NAME+"/package.json",JSON.stringify(packageJson, null, 4 )));
 
 
-app.listen(3000, ()=>{
-	console.log("Server running at port 3000");
-});
-`
-createFile(PROJECT_NAME+"/index.js",indexFile);
+		//Create directories for Basic project Structure
+		console.log(await createDirectory(PROJECT_NAME+"/routes"));
+		console.log(await createDirectory(PROJECT_NAME+"/public"));
+
+		//Create index.js
+
+		indexFile = `
+		const express = require("express");
+		const app = express();
+
+		const morgan = require("morgan"); //logging -> express.logger is deprecated.
+
+		//Routes
+		const indexRoute = require("./routes/index");
+		const usersRoute = require("./routes/users");
+
+		//Middlewares
+		app.use(express.json());
+		app.use(morgan("tiny"));
+		app.use(express.static('public'));
+
+		//Route Middlewares
+		app.use("/", indexRoute);
+		app.use("/users", usersRoute);
 
 
-//Create indexRoute .. routes/index.js
-indexRouteFile = `
-const router = require("express").Router();
-
-router.get("/", (req,res) => {
-	res.send("Welcome to custom Express Generator :)");
-});
-
-module.exports = router;
-`
-createFile(PROJECT_NAME+"/routes/index.js", indexRouteFile);
-
-//create usersRoute .. routes/users.js
-usersRouteFile = `
-const router = require("express").Router();
-
-router.get("/", (req,res) => {
-	res.send("Serve resources to the users ..");
-	//res.json({msg:"Or Share JSON datas"});
-});
-
-module.exports = router;
-`
-createFile(PROJECT_NAME+"/routes/users.js", usersRouteFile);
+		app.listen(3000, ()=>{
+			console.log("Server running at port 3000");
+		});
+		`
+		console.log(await createFile(PROJECT_NAME+"/index.js",indexFile));
 
 
-console.log("PROJECT "+PROJECT_NAME+" Created !");
-console.log("$ cd "+PROJECT_NAME+"\n$ npm install\n$ npm start");
-console.log("start script uses nodemon so if u dont have it you can install it globally using\n$ npm install -g nodemon");
+		//Create indexRoute .. routes/index.js
+		indexRouteFile = `
+		const router = require("express").Router();
+
+		router.get("/", (req,res) => {
+			res.send("Welcome to custom Express Generator :)");
+		});
+
+		module.exports = router;
+		`
+		console.log(await createFile(PROJECT_NAME+"/routes/index.js", indexRouteFile));
+
+		//create usersRoute .. routes/users.js
+		usersRouteFile = `
+		const router = require("express").Router();
+
+		router.get("/", (req,res) => {
+			res.send("Serve resources to the users ..");
+			//res.json({msg:"Or Share JSON datas"});
+		});
+
+		module.exports = router;
+		`
+		console.log(await createFile(PROJECT_NAME+"/routes/users.js", usersRouteFile));
+
+		console.log("-------------------------------------------------------------------------------------");
+		console.log("PROJECT "+PROJECT_NAME+" Created ! Now You can");
+		console.log("-------------------------------------------------------------------------------------");
+		console.log("\n$ cd "+PROJECT_NAME+"\n\n$ npm install\n\n$ npm start\n");
+		console.log("-------------------------------------------------------------------------------------");
+		console.log("start script uses nodemon so if u dont have it you can install it globally using\n$ npm install -g nodemon");
+
+	}catch(err) {
+		console.log("ERR://"+err);		
+	}
+})();
